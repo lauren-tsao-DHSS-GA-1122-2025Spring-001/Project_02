b7bc7c0fa9b0014ff17ebf6c0787e3f9aa15e409
Lauren Tsao <et2676@nyu.edu>
2025-03-27 13:48:54 -0400

    updated code

--- script.js
+++ script.js
@@ -1,50 +1,30 @@
-$(document).ready(function() {
-    var $filterCheckboxes = $('input[type="checkbox"]');
-  
-    var filterFunc = function() {
-      var selectedFilters = {};
-  
-      $filterCheckboxes.filter(':checked').each(function() {
-        if (!selectedFilters.hasOwnProperty(this.name)) {
-          selectedFilters[this.name] = [];
-        }
-        selectedFilters[this.name].push(this.value);
-      });
-  
-      var $filteredResults = $('.magnet');
-  
-      $.each(selectedFilters, function(name, filterValues) {
-        $filteredResults = $filteredResults.filter(function() {
-          var matched = false,
-              currentFilterValues = $(this).data('category').split(' ');
-  
-          $.each(currentFilterValues, function(_, currentFilterValue) {
-            if ($.inArray(currentFilterValue, filterValues) !== -1) {
-              matched = true;
-              return false;
-            }
-          });
-  
-          return matched;
-        });
-      });
-  
-      // Show only the filtered results
-      $filteredResults.show(); 
-      // Hide non-matching results
-      $('.magnet').not($filteredResults).hide(); 
-    };
-  
-    // Attach the filter function to checkbox change event
-    $filterCheckboxes.on('change', filterFunc);
-  
-    // Clear all checkboxes and reset filter
-    function clearFilters() {
-      $filterCheckboxes.prop('checked', false); // Uncheck all checkboxes
-      filterFunc(); // Reapply the filter (this will show all magnets)
-    }
-  
-    // Attach the clearFilters function to the "Clear all" button
-    $(".clear-btn").on("click", clearFilters);
-  });
-  
+let asiaBox = document.getElementById("asia");
+
+let europeBox = document.getElementById("europe");
+
+let n_americaBox = document.getElementById("n-america");
+
+let metalBox = document.getElementById("metal");
+
+let ceramicBox = document.getElementById("ceramic");
+
+let mQatar = document.getElementById("mQatar");
+
+let mMontreal = document.getElementById("mMontreal");
+
+function filter() {
+
+// Qatar magnet visibility  
+  if (asiaBox.checked == true || metalBox.checked == true) {
+    mQatar.style.display = "block";
+  } else {
+    mQatar.style.display = "none";
+  }
+
+  // Montreal magnet visibility
+  if (n_americaBox.checked == true || ceramicBox.checked == true) {
+    mMontreal.style.display = "block";
+  } else {
+    mMontreal.style.display = "none";
+  }
+}
